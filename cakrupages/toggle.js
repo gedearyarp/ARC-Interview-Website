6abcc6d91fa8eab037fbbdae584ca6e99c41e994
GedeAryaRP <71829426+gedearyarp@users.noreply.github.com>
2021-03-06 23:41:58 +0700

    Add Files

---
+++ cakrupages/toggle.js
@@ -0,0 +1,17 @@
+function darkfunc() {
+    document.documentElement.style.setProperty('--bg', '#262626');
+    document.documentElement.style.setProperty('--huruf', '#eeeeee');
+    document.documentElement.style.setProperty('--bgkontener', '#262626');
+    document.documentElement.style.setProperty('--shadow', '20px 20px 60px #1d1d1d');
+    document.documentElement.style.setProperty('--bgbutton', 'black');
+    document.documentElement.style.setProperty('--hurufbutton', '#ffffff');
+}
+function lightfunc() {
+    document.documentElement.style.setProperty('--bg', '#e6e6e6');
+    document.documentElement.style.setProperty('--huruf', '#222222');
+    document.documentElement.style.setProperty('--bgkontener', '#f2f2f2');
+    document.documentElement.style.setProperty('--shadow', '20px 20px 60px #cacaca');
+    document.documentElement.style.setProperty('--bgbutton', '#ffffff');
+    document.documentElement.style.setProperty('--hurufbutton', '#333333');
+}
+//document.documentElement.style.setProperty('--your-variable', '#YOURCOLOR');
---
+++ toggle.js
@@ -0,0 +1,22 @@
+function darkfunc() {
+    document.documentElement.style.setProperty('--bg', '#262626');
+    document.documentElement.style.setProperty('--huruf', '#eeeeee');
+    document.documentElement.style.setProperty('--bgkontener', '#262626');
+    document.documentElement.style.setProperty('--shadow', '20px 20px 60px #1d1d1d');
+    document.documentElement.style.setProperty('--bgbutton', 'black');
+    document.documentElement.style.setProperty('--hurufbutton', '#ffffff');
+    document.documentElement.style.setProperty('--bg_hw', '#72faca');
+    document.documentElement.style.setProperty('--huruf_hw', '#404040');
+    document.documentElement.style.setProperty('--ae_hw', '#68e2b7');
+}
+function lightfunc() {
+    document.documentElement.style.setProperty('--bg', '#e6e6e6');
+    document.documentElement.style.setProperty('--huruf', '#222222');
+    document.documentElement.style.setProperty('--bgkontener', '#ffffff');
+    document.documentElement.style.setProperty('--shadow', '20px 20px 60px #cacaca');
+    document.documentElement.style.setProperty('--bgbutton', '#ffffff');
+    document.documentElement.style.setProperty('--hurufbutton', '#333333');
+    document.documentElement.style.setProperty('--bg_hw', '#e6ecff');
+    document.documentElement.style.setProperty('--huruf_hw', '#3363ff');
+    document.documentElement.style.setProperty('--ae_hw', '#dce4ff');
+}
